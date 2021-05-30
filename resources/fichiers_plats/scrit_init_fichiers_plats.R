f10500c174803c14f9435af446e4ed45fae8e23d
Pierre-Yves Berrard <pierre-yves.berrard@insee.fr>
2021-05-31 01:02:56 +0200

    simplifie script init fichiers plats

--- resources/fichiers_plats/scrit_init_fichiers_plats.R
+++ resources/fichiers_plats/scrit_init_fichiers_plats.R
@@ -5,27 +5,16 @@ unlink("fichiers_plats", recursive = TRUE)
 dir.create("fichiers_plats")
 
 # Télécharger les données grâce à doremifasol
-doremifasol::telechargerFichier(
-  "BPE_ENS", 
-  telDir = "fichiers_plats")
+options(doremifasol.telDir = "fichiers_plats")
+doremifasol::telechargerFichier("BPE_ENS")
+doremifasol::telechargerFichier("DECES_COM_0918")
 
-doremifasol::telechargerFichier(
-  "DECES_COM_0918", 
-  telDir = "fichiers_plats")
-
-# Lister les fichiers et dézipper si besoin
-lapply(
-  list.files("fichiers_plats"), 
-  function(x) {
-    print(paste0("On traite le fichier : ", x))
-    if (stringr::str_detect(x, "zip$")) {
-      utils::unzip(
-        paste0("fichiers_plats", "/", x), 
-        overwrite = TRUE, 
-        exdir = "fichiers_plats"
-      )
-    } else {
-      return(NULL)
-    }
-  }
+# Décompresser les fichiers .zip
+invisible(
+  lapply(
+    list.files("fichiers_plats", pattern = "\\.zip$", full.names = TRUE), 
+    utils::unzip,
+    overwrite = TRUE, 
+    exdir = "fichiers_plats"
+  )
 )
